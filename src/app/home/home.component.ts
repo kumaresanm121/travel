e08c0c8380898db004cb9e696eef58f43c025891
kumaresan <kumaresan.m@tristhaglobal.com>
2020-12-06 10:00:43 +0530

    car booking details updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule ,FormControl,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,16 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  carbook ={   checkin:new Date(Date.now()).toLocaleString('en-US'),checkout:new Date(Date.now()).toLocaleString('en-US'),
+    start:String,end:String,duration:1,contact:String,
+    email:String,members:2,child:Number,adult:Number,name:String,package:String}
+  submitbooking(){
+    console.log(this.carbook);
+  }
+
   ngOnInit(): void {
+
+    
   }
 
 }
