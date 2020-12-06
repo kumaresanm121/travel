4cf7839316006d028c14c1eeb85b2dcfadbb6351
adhityamurali <adhityamurali03@gmail.com>
2020-12-06 18:45:22 +0530

    emailjs integration

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
+
 export class HeaderComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log('sending email')
   }
+
+  public sendEmail(e: Event) {
+    console.log('dddd')
+    e.preventDefault();
+    console.log(e)
+
+    var templateParams = {
+      from_name: 'James',
+      to_name:'Mike',
+      message: 'Check this out!'
+  };
+
+    emailjs.send('service_12nppnf', 'template_lddpctq', templateParams, 'user_hDJsZNSAaPkIw7bfX766z')
+    
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+
+  
 
 }
