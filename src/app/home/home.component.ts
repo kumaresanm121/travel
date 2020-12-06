71e95accdbc7eda44412608c13c217165e722e8d
kumaresan <kumaresan.m@tristhaglobal.com>
2020-12-06 13:02:14 +0530

    updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,8 @@ export class HomeComponent implements OnInit {
 
   carbook ={   checkin:new Date(Date.now()).toLocaleDateString(),checkout:new Date(Date.now()).toLocaleDateString(),
     start:null,end:null,contact:null,
-    email:null,child:0,adult:0,name:String,package:String}
+    email:null,child:0,adult:0,name:null,package:String}
+  isActive = 1;  
   submitbooking(){
     console.log(this.carbook);
   }
